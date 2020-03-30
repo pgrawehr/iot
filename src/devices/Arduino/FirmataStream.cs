f1a379a367fb3c760331e8b0d68fefe7adf96951
Patrick Grawehr <pgrawehr@hotmail.com>
2020-03-30 10:48:55 +0200

    First draft of Firmata/Arduino binding

---
+++ src/devices/Arduino/FirmataStream.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Iot.Device.Arduino
+{
+    /// <summary>
+    /// Input/Output stream to a Firmata-Enabled device
+    /// </summary>
+    public abstract class FirmataStream : Stream
+    {
+        public void flush();
+
+        public void end();
+
+        public void write(byte b);
+
+        public ushort read();
+    }
+}
