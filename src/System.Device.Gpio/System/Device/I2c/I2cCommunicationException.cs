0de2914ea469cbe225efc40056e804c181dcea96
Patrick Grawehr <pgrawehr@hotmail.com>
2020-04-03 18:39:21 +0200

    Add separate I2cCommunicationException

---
+++ src/System.Device.Gpio/System/Device/I2c/I2cCommunicationException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace System.Device.Gpio.I2c
+{
+    /// <summary>
+    /// This exception is thrown on I2C communication errors
+    /// </summary>
+    public class I2cCommunicationException : IOException
+    {
+        public I2cCommunicationException()
+        {
+        }
+
+        public I2cCommunicationException(string message)
+            : base(message)
+        {
+        }
+
+        public I2cCommunicationException(string message, System.Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        protected I2cCommunicationException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context)
+            : base(info, context)
+        {
+
+        }
+    }
+}
