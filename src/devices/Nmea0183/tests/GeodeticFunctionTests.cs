2e8e051de99929d6ba2d3d3420730f38cfd5b26e
Patrick Grawehr <pgrawehr@hotmail.com>
2020-04-29 08:05:34 +0200

    A bit of cleanup

---
+++ src/devices/Nmea0183/tests/GeodeticFunctionTests.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Units;
+using Xunit;
+
+namespace Iot.Device.Nmea0183.Tests
+{
+    public class DistAndDirTests
+    {
+        /// <summary>
+        /// Returns a random latitude value.
+        /// </summary>
+        /// <param name="r">Random number generator</param>
+        /// <returns>A value between -80 and +80, some computations get inexact close to the poles</returns>
+        private double RandomLatitude(Random r)
+        {
+            double v = r.NextDouble();
+            return (v * 160) - 80;
+        }
+
+        /// <summary>
+        /// Returns a value between -180 and +180.
+        /// </summary>
+        /// <param name="r">Random number generator</param>
+        /// <returns>-180 to 180</returns>
+        private double RandomLongitude(Random r)
+        {
+            double v = r.NextDouble();
+            return (v * 360) - 180;
+        }
+
+        // Really random values would point out problems, but there are some input values True cause the test to fail
+        // because the error is big for very large distances.
+        // [TestCase(4711)] // This gets a pair of values where DistAndDir does not converge.
+        [Theory]
+        [InlineData(32102)]
+        [InlineData(45120)]
+        public void WGS84Test(int startV)
+        {
+            Random rand = new Random(startV);
+            System.Diagnostics.Trace.WriteLine("Starting Test for value " + startV.ToString(CultureInfo.CurrentCulture));
+            int nErrors = 0;
+            for (int i = 0; i < 10000; i++)
+            {
+                double startlat = RandomLatitude(rand);
+                double startlon = RandomLongitude(rand);
+                double endlat = RandomLatitude(rand);
+                double endlon = RandomLongitude(rand);
+                if (!TestDistAndDir(startlat, startlon, endlat, endlon))
+                {
+                    nErrors++;
+                }
+            }
+
+            Assert.True(nErrors == 0);
+        }
+
+        [Fact]
+        public void WGS84Test_Statics()
+        {
+            Assert.True(TestDistAndDir(0, 180, 0, -180));
+            Assert.True(TestDistAndDir(0, -179, 0, 179));
+            Assert.True(TestDistAndDir(45, 10, -55, -170));
+            Assert.True(TestDistAndDir(-35.84, 94, 35.87, -85));
+            // These are (why?) special cases that get inexact releases.
+            // Assert.True(TestDistAndDir(35.8725750473666, -85.696011868164,
+            //    - 35.8447055545844, 94.3611548069683));
+            // Assert.True(TestDistAndDir(-35.8447055545844, 94.3611548069683,
+            //    35.8725750473666, -85.696011868164));
+            double dist = 0;
+            double dir = 0;
+            GreatCircle.DistAndDir(47, 9, 47, 9, out dist, out dir);
+            Assert.Equal(0, dist);
+            double dist2 = 0;
+            double dir2 = 0;
+            GreatCircle.DistAndDir(-35.84, 94,
+                35.87, -85, out dist, out dir);
+            GreatCircle.DistAndDir(-35.840000001, 94,
+                35.87, -85, out dist2, out dir2);
+            Assert.True(dist2 > dist);
+            GreatCircle.DistAndDir(-35.84, 94.000001,
+                35.87, -85, out dist2, out dir2);
+            Assert.True(dist2 > dist);
+
+        }
+
+        private static bool TestDistAndDir(double startlat, double startlon, double endlat, double endlon)
+        {
+            double dist = 0;
+            double dir = 0;
+            GreatCircle.DistAndDir(startlat, startlon, endlat, endlon, out dist, out dir);
+
+            Angle dirNormalized = Angle.FromDegrees(dir).Normalize(true);
+            if (endlon >= 180)
+            {
+                endlon -= 360;
+            }
+
+            if (endlon < -180)
+            {
+                endlon += 360;
+            }
+
+            Assert.True(dist < 30000000, "Distance more than 1/2 times around the earth");
+            Assert.True(dirNormalized.Degrees >= 0 && dirNormalized.Degrees < 360, "Angle out of bounds");
+            double reslat = 0;
+            double reslon = 0;
+            GreatCircle.CalcCoords(startlat, startlon, dir, dist, out reslat, out reslon);
+            Angle lon = Angle.FromDegrees(reslon);
+            lon = lon.Normalize(false);
+            Assert.True(reslat >= -90 && reslat <= 90, "Invalid latitude");
+            Assert.True(lon.Degrees > -180, $"Invalid longitude A {reslon}");
+            Assert.True(lon.Degrees <= 180, $"Invalid longitude B {reslon}");
+            double accuracy = 1E-7;
+            if (dist > 100000)
+            {
+                accuracy = 1E-6;
+            }
+
+            if (dist > 1000000)
+            {
+                accuracy = 1E-6;
+            }
+
+            if (dist > 10000000)
+            {
+                accuracy = 1E-5;
+            }
+
+            if (Math.Abs(endlat - reslat) > accuracy)
+            {
+                Console.WriteLine("Error testing: " + startlat + "/" + startlon +
+                                                   " to " + endlat + "/" + endlon + " Distance: " + dist +
+                                                   " Difference in lat: " + (endlat - reslat));
+                return false;
+            }
+
+            if (GreatCircle.AngleDifferenceSignedDegrees(endlon, lon.Degrees) > accuracy)
+            {
+                Console.WriteLine("Error testing: " + startlat + "/" + startlon +
+                                                   " to " + endlat + "/" + endlon + " Distance: " + dist +
+                                                   " Difference in lon: " + (endlon - reslon));
+                return false;
+            }
+
+            return true;
+        }
+
+        private static void InternalDistDir(GeographicPosition startPosition,
+            GeographicPosition endPosition,
+            ref double distance, ref double direction)
+        {
+            double deltaLat = endPosition.Latitude - startPosition.Latitude;
+            double deltaLon = endPosition.Longitude - startPosition.Longitude;
+            double deltaX = deltaLon * GreatCircle.METERS_PER_DEGREE_LONGITUDE * Math.Abs(Math.Cos(startPosition.Latitude * Math.PI / 180.0));
+            double deltaY = deltaLat * GreatCircle.METERS_PER_DEGREEE_LATITUDE;
+
+            distance = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+            direction = GreatCircle.RadiansToAviatic(Math.Atan2(deltaY, deltaX));
+        }
+
+        public static GeographicPosition InternalExtrapolatePosition(
+            GeographicPosition originalPosition,
+            double dblDist, double dblAngle)
+        {
+            double db_i;
+            double dl_i;
+            double alt, lat, lon;
+
+            db_i = (Math.Cos(Math.PI / 180.0 * dblAngle) * (dblDist)) / GreatCircle.METERS_PER_DEGREEE_LATITUDE;
+            lat = originalPosition.Latitude + db_i;
+            dl_i = (Math.Sin(Math.PI / 180.0 * dblAngle) * (dblDist)) / (GreatCircle.METERS_PER_DEGREE_LONGITUDE * (Math.Abs(Math.Cos(lat * Math.PI / 180.0))));
+            lon = originalPosition.Longitude + dl_i;
+            alt = originalPosition.EllipsoidalHeight;
+            return new GeographicPosition(lat, lon, alt);
+        }
+
+        [Fact]
+        public void RadiansToAviaticTest()
+        {
+            Assert.Equal(90, GreatCircle.RadiansToAviatic(0));
+            Assert.Equal(0, GreatCircle.RadiansToAviatic(Math.PI / 2.0));
+            Assert.Equal(270, GreatCircle.RadiansToAviatic(Math.PI));
+            Assert.Equal(270, GreatCircle.RadiansToAviatic(-Math.PI));
+            Assert.Equal(180, GreatCircle.RadiansToAviatic(-Math.PI / 2.0));
+        }
+
+        [Fact]
+        public void AviaticToRadiansTest()
+        {
+            Assert.Equal(0, GreatCircle.AviaticToRadians(90));
+            Assert.Equal(Math.PI / 2.0, GreatCircle.AviaticToRadians(0));
+            Assert.Equal(Math.PI, GreatCircle.AviaticToRadians(270));
+            Assert.Equal(Math.PI, GreatCircle.AviaticToRadians(-90));
+            Assert.Equal(Math.PI / 2.0 * 3, GreatCircle.AviaticToRadians(180));
+        }
+
+        // this is a test for the accuracy of the great circle calculation for small distances,
+        // it compares a cartesian calculation result with the GC calculation results
+        [Fact]
+        public void TestCartesianToGC()
+        {
+            // start position of test
+            double latStart = 47.0;
+            double lonStart = 11.0;
+
+            // 1 arcsec is about 30m => move in 0.3m steps along all 8 axis away from the start position
+            double deltaInc = 0.01 / 3600.0;
+
+            GeographicPosition pStart = new GeographicPosition(latStart, lonStart, 0);
+            double dblDist = 0;
+            double dblDir = 0;
+            double dblGCDist = 0;
+            double dblGCDir = 0;
+
+            // iterate over the 8 axis (45° increments)
+            for (int signIdx = 0; signIdx < 8; signIdx++)
+            {
+                double dblXSign = 1.0;
+                if (signIdx >= 4 && signIdx <= 6)
+                {
+                    dblXSign = -1.0;
+                }
+                else if (signIdx == 3 || signIdx == 7)
+                {
+                    dblXSign = 0.0;
+                }
+
+                double dblYSign = 1.0;
+                if (signIdx >= 2 && signIdx <= 4)
+                {
+                    dblYSign = -1.0;
+                }
+                else if (signIdx == 1 || signIdx == 5)
+                {
+                    dblYSign = 0.0;
+                }
+
+                // iterate in 0.3m steps (up to about 150m distance)
+                for (int idx = 1; idx <= 400; idx++)
+                {
+                    double delta = idx * deltaInc;
+                    GeographicPosition pEnd = new GeographicPosition(latStart + dblYSign * delta, lonStart + dblXSign * delta, 0);
+                    InternalDistDir(pStart, pEnd, ref dblDist, ref dblDir);
+                    GreatCircle.DistAndDir(pStart, pEnd, out dblGCDist, out dblGCDir);
+
+                    // compare the two calculation methods
+                    Assert.True(Math.Abs(dblDist - dblGCDist) < 1.0, "position accuracy less than 1m");
+                    Assert.True(GreatCircle.AngleDifferenceSignedDegrees(dblDir, dblGCDir) < 1.0, "direction accuracy less than 1 deg");
+
+                    // calculate the endpoint with the previously calculated offsets using great circle
+                    double dblEndLat = 0;
+                    double dblEndLon = 0;
+                    GreatCircle.CalcCoords(pStart.Latitude, pStart.Longitude, dblGCDir, dblGCDist, out dblEndLat, out dblEndLon);
+                    Assert.True(Math.Abs(dblEndLat - pEnd.Latitude) < 1.0, "GC latitude accuracy less than 1m");
+                    Assert.True(GreatCircle.AngleDifferenceSignedDegrees(dblEndLon, pEnd.Longitude) < 1.0, "GC longitude accuracy less than 1m");
+
+                    // calculate the endpoint with the previously calculated offsets using the cartesic routine
+                    GeographicPosition pCalcEnd = InternalExtrapolatePosition(pStart, dblDist, dblDir);
+                    double dblDeltaX = Math.Abs(pCalcEnd.Longitude - pEnd.Longitude) * GreatCircle.METERS_PER_DEGREE_LONGITUDE;
+                    double dblDeltaY = Math.Abs(pCalcEnd.Latitude - pEnd.Latitude) * GreatCircle.METERS_PER_DEGREEE_LATITUDE;
+                    Assert.True(dblDeltaY < 1.0, "XY latitude accuracy less than 1m");
+                    Assert.True(dblDeltaX < 1.0, "XY longitude accuracy less than 1m");
+                }
+            }
+        }
+    }
+}
---
+++ src/devices/Units/GreatCircle.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#pragma warning disable CS1591
+namespace Units
+{
+    public static class GreatCircle
+    {
+        /// <summary>
+        /// Semi-Mayor Axis of the WGS ellipsoid
+        /// </summary>
+        public const double WGS84_A = 6378137.0;
+
+        /// <summary>
+        /// Flattening "f" of the WGS84 ellipsoid (1.0 / 298.25722357)
+        /// </summary>
+        public const double WGS84_F = 0.00335281066474;
+
+        /// <summary>
+        /// m per degree latitude
+        /// </summary>
+        public const double METERS_PER_DEGREEE_LATITUDE = 110563.789;
+
+        /// <summary>
+        /// m per degree longitude on the equator
+        /// </summary>
+        public const double METERS_PER_DEGREE_LONGITUDE = 111312.267;
+
+        private static GeoidCalculations.geod_geodesic _geod;
+
+        static GreatCircle()
+        {
+            GeoidCalculations.geod_init(out _geod, WGS84_A, WGS84_F);
+        }
+
+        public static void DistAndDir(GeographicPosition position1, GeographicPosition position2, out double distance, out double direction)
+        {
+            DistAndDir(position1.Latitude, position1.Longitude, position2.Latitude, position2.Longitude, out distance, out direction);
+        }
+
+        public static void DistAndDir(double latitude1,  double longitude1, double latitude2, double longitude2, out double distance, out double direction)
+        {
+            GeoidCalculations.geod_inverse(_geod, latitude1, longitude1, latitude2, longitude2, out distance, out direction, out _);
+        }
+
+        public static void CalcCoords(double startLatitude, double startLongitude, double direction, double distance, out double resultLatitude, out double resultLongitude)
+        {
+            GeoidCalculations.geod_direct(_geod, startLatitude, startLongitude, direction, distance, out resultLatitude, out resultLongitude, out _);
+        }
+
+        /// <summary>
+        /// Convert a value to radians.
+        /// </summary>
+        public static double DegreesToRadians(double val)
+        {
+            double ret = ((2 * Math.PI * val) / 360);
+            return ret;
+        }
+
+        /// <summary>
+        /// Converts an angle in aviatic definition to mathematic definition.
+        /// Aviatic angles are in degrees, where 0 degrees is north, counting clockwise, mathematic angles
+        /// are in radians, starting east and going counterclockwise.
+        /// </summary>
+        /// <param name="val">Aviatic angle, degrees</param>
+        /// <returns>Mathematic angle, radians, fast-normalized to 0..2Pi</returns>
+        public static double AviaticToRadians(double val)
+        {
+            double ret = ((-val) + 90.0);
+            ret = DegreesToRadians(ret);
+            if (ret >= 2 * Math.PI)
+            {
+                ret -= 2 * Math.PI;
+            }
+
+            if (ret < 0)
+            {
+                ret += 2 * Math.PI;
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Calculate the difference between two angles, return degrees.
+        /// </summary>
+        /// <param name="a">First angle, in degrees</param>
+        /// <param name="b">Second angle, in degrees</param>
+        /// <returns>Difference, ranging -180 to +180, in degrees.</returns>
+        public static double AngleDifferenceSignedDegrees(double a, double b)
+        {
+            double val = a - b;
+            if (val > 180)
+            {
+                val -= 360;
+            }
+
+            if (val < -180)
+            {
+                val += 360;
+            }
+
+            return val;
+        }
+
+        /// <summary>
+        /// Converts an angle in radians to angle in decimal degrees
+        /// </summary>
+        /// <param name="radians">Angle in radians (0-2*Pi)</param>
+        /// <returns>Angle in decimal degrees (0-360)</returns>
+        public static double RadiansToDegrees(double radians)
+        {
+            return radians * 180.0 / Math.PI;
+        }
+
+        /// <summary>
+        /// Convert angle from mathematic to aviatic.
+        /// See also AviaticToRadians()
+        /// </summary>
+        /// <param name="val">Mathematic value in radians</param>
+        /// <returns>Aviatic value in degrees</returns>
+        public static double RadiansToAviatic(double val)
+        {
+            double ret = RadiansToDegrees(val);
+            ret = ((-ret) + 90.0);
+            if (ret < 0)
+            {
+                ret += 360;
+            }
+
+            return ret;
+        }
+    }
+}
