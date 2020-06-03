aa52a23c56f4b3269fb84b8a32cb26991cd06543
Patrick Grawehr <pgrawehr@hotmail.com>
2020-06-03 20:55:06 +0200

    This is ugly - subclassing all classes to call their methods...?

---
+++ src/devices/Board/IGpioDriver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Gpio;
+using System.Text;
+using System.Threading;
+
+namespace Iot.Device.Board
+{
+    public interface IGpioDriver
+    {
+        int PinCount
+        {
+            get;
+        }
+
+        void OpenPin(int pinNumber);
+        void ClosePin(int pinNumber);
+
+        void SetPinMode(int pinNumber, PinMode mode);
+
+        PinMode GetPinMode(int pinNumber);
+
+        bool IsPinModeSupported(int pinNumber, PinMode mode);
+
+        PinValue Read(int pinNumber);
+
+        void Write(int pinNumber, PinValue value);
+
+        WaitForEventResult WaitForEvent(int pinNumber, PinEventTypes eventTypes, CancellationToken cancellationToken);
+
+        void AddCallbackForPinValueChangedEvent(int pinNumber, PinEventTypes eventTypes, PinChangeEventHandler callback);
+
+        void RemoveCallbackForPinValueChangedEvent(int pinNumber, PinChangeEventHandler callback);
+    }
+}
---
+++ src/devices/Board/ManagedGpioDriver.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Gpio;
+using System.Text;
+using System.Threading;
+
+namespace Iot.Device.Board
+{
+    public class ManagedGpioDriver : GpioDriver, IGpioDriver
+    {
+        private readonly BoardBase _board;
+        private IGpioDriver _gpioDriverImplementation;
+
+        public ManagedGpioDriver(BoardBase board, IGpioDriver actualDriver)
+        {
+            _board = board;
+            _gpioDriverImplementation = actualDriver;
+        }
+
+        int IGpioDriver.PinCount => PinCount;
+
+        protected override int PinCount
+        {
+            get
+            {
+                return _gpioDriverImplementation.PinCount;
+            }
+        }
+
+        protected override int ConvertPinNumberToLogicalNumberingScheme(int pinNumber)
+        {
+            return _board.ConvertPinNumberToLogicalNumberingScheme(pinNumber);
+        }
+
+        void IGpioDriver.OpenPin(int pinNumber)
+        {
+            OpenPin(pinNumber);
+        }
+
+        protected override void OpenPin(int pinNumber)
+        {
+
+            _gpioDriverImplementation.OpenPin(pinNumber);
+        }
+
+        void IGpioDriver.ClosePin(int pinNumber)
+        {
+            ClosePin(pinNumber);
+        }
+
+        protected override void ClosePin(int pinNumber)
+        {
+            _gpioDriverImplementation.ClosePin(pinNumber);
+        }
+
+        protected override void SetPinMode(int pinNumber, PinMode mode)
+        {
+            _gpioDriverImplementation.SetPinMode(pinNumber, mode);
+        }
+
+        protected override PinMode GetPinMode(int pinNumber)
+        {
+            return _gpioDriverImplementation.GetPinMode(pinNumber);
+        }
+
+        protected override bool IsPinModeSupported(int pinNumber, PinMode mode)
+        {
+            return _gpioDriverImplementation.IsPinModeSupported(pinNumber, mode);
+        }
+
+        protected override PinValue Read(int pinNumber)
+        {
+            return _gpioDriverImplementation.Read(pinNumber);
+        }
+
+        protected override void Write(int pinNumber, PinValue value)
+        {
+            _gpioDriverImplementation.Write(pinNumber, value);
+        }
+
+        protected override WaitForEventResult WaitForEvent(int pinNumber, PinEventTypes eventTypes, CancellationToken cancellationToken)
+        {
+            return _gpioDriverImplementation.WaitForEvent(pinNumber, eventTypes, cancellationToken);
+        }
+
+        protected override void AddCallbackForPinValueChangedEvent(int pinNumber, PinEventTypes eventTypes, PinChangeEventHandler callback)
+        {
+            _gpioDriverImplementation.AddCallbackForPinValueChangedEvent(pinNumber, eventTypes, callback);
+        }
+
+        protected override void RemoveCallbackForPinValueChangedEvent(int pinNumber, PinChangeEventHandler callback)
+        {
+            _gpioDriverImplementation.RemoveCallbackForPinValueChangedEvent(pinNumber, callback);
+        }
+
+        void IGpioDriver.SetPinMode(int pinNumber, PinMode mode)
+        {
+            SetPinMode(pinNumber, mode);
+        }
+
+        PinMode IGpioDriver.GetPinMode(int pinNumber)
+        {
+            return GetPinMode(pinNumber);
+        }
+
+        bool IGpioDriver.IsPinModeSupported(int pinNumber, PinMode mode)
+        {
+            return IsPinModeSupported(pinNumber, mode);
+        }
+
+        PinValue IGpioDriver.Read(int pinNumber)
+        {
+            return Read(pinNumber);
+        }
+
+        void IGpioDriver.Write(int pinNumber, PinValue value)
+        {
+            Write(pinNumber, value);
+        }
+
+        WaitForEventResult IGpioDriver.WaitForEvent(int pinNumber, PinEventTypes eventTypes, CancellationToken cancellationToken)
+        {
+            return WaitForEvent(pinNumber, eventTypes, cancellationToken);
+        }
+
+        void IGpioDriver.AddCallbackForPinValueChangedEvent(int pinNumber, PinEventTypes eventTypes, PinChangeEventHandler callback)
+        {
+            AddCallbackForPinValueChangedEvent(pinNumber, eventTypes, callback);
+        }
+
+        void IGpioDriver.RemoveCallbackForPinValueChangedEvent(int pinNumber, PinChangeEventHandler callback)
+        {
+            RemoveCallbackForPinValueChangedEvent(pinNumber, callback);
+        }
+    }
+}
