4d537ec1a74e61058c10fa9dcdd8fe43e3bbd17d
Patrick Grawehr <patrick.grawehr@leica-geosystems.com>
2020-09-03 20:28:50 +0200

    Add unit tests

--- src/devices/Common/tests/HistoryOperationTests.cs
+++ src/devices/Common/tests/HistoryOperationTests.cs
@@ -16,5 +16,26 @@ namespace Iot.Device.Common.Tests
             var list = new List<HistoricValue>() { h1, h2 };
             Assert.Equal(Speed.FromMetersPerSecond(2), list.MaxValue());
         }
+
+        [Fact]
+        public void Minimum()
+        {
+            HistoricValue h1 = new HistoricValue(DateTime.Now, Speed.FromMetersPerSecond(1));
+            HistoricValue h2 = new HistoricValue(DateTime.Now, Speed.FromMetersPerSecond(2));
+            var list = new List<HistoricValue>() { h1, h2 };
+            Assert.Equal(Speed.FromMetersPerSecond(1), list.MinValue());
+        }
+
+        [Fact]
+        public void ConvertToAges()
+        {
+            DateTime now = DateTime.Now;
+            HistoricValue h1 = new HistoricValue(now, Speed.FromMetersPerSecond(1));
+            HistoricValue h2 = new HistoricValue(now - TimeSpan.FromSeconds(1), Speed.FromMetersPerSecond(2));
+            var list = new List<HistoricValue>() { h1, h2 };
+            var result = list.ConvertToAges(now);
+            Assert.Equal(TimeSpan.Zero, result[0].Age);
+            Assert.Equal(TimeSpan.FromSeconds(1), result[1].Age);
+        }
     }
 }
