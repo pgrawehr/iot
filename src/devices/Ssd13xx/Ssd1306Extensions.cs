a193c7ac929f97f6770ce02d35929a11a271b2b1
Patrick Grawehr <pgrawehr@hotmail.com>
2021-05-20 09:00:51 +0200

    Move extension methods to library
    These functions offer important functionality,
    so they shouldn't be just in the example

---
+++ src/devices/Ssd13xx/Ssd1306Extensions.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Iot.Device.Ssd13xx.Commands;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace Iot.Device.Ssd13xx
+{
+    /// <summary>
+    /// Extension methods for Ssd1306 class.
+    /// </summary>
+    public static class Ssd13xxExtensions
+    {
+        // Port from https://github.com/adafruit/Adafruit_Python_SSD1306/blob/8819e2d203df49f2843059d981b7347d9881c82b/Adafruit_SSD1306/SSD1306.py#L184
+
+        /// <summary>
+        /// Extension method to display image using Ssd1306 device.
+        /// </summary>
+        /// <param name="s">Ssd1306 object.</param>
+        /// <param name="image">Image to display.</param>
+        public static void DisplayImage(this Ssd1306 s, Image<L16> image)
+        {
+            Int16 width = 128;
+            Int16 pages = 4;
+            List<byte> buffer = new();
+
+            for (int page = 0; page < pages; page++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    int bits = 0;
+                    for (byte bit = 0; bit < 8; bit++)
+                    {
+                        bits = bits << 1;
+                        bits |= image[x, page * 8 + 7 - bit].PackedValue > 0 ? 1 : 0;
+                    }
+
+                    buffer.Add((byte)bits);
+                }
+            }
+
+            int chunk_size = 16;
+            for (int i = 0; i < buffer.Count; i += chunk_size)
+            {
+                s.SendData(buffer.Skip(i).Take(chunk_size).ToArray());
+            }
+        }
+
+        /// <summary>
+        /// Initializes the display for 128x32 pixels
+        /// </summary>
+        public static void Initialize(this Ssd1306 device)
+        {
+            device.SendCommand(new SetDisplayOff());
+            device.SendCommand(new Commands.Ssd1306Commands.SetDisplayClockDivideRatioOscillatorFrequency(0x00, 0x08));
+            device.SendCommand(new SetMultiplexRatio(0x1F));
+            device.SendCommand(new Commands.Ssd1306Commands.SetDisplayOffset(0x00));
+            device.SendCommand(new Commands.Ssd1306Commands.SetDisplayStartLine(0x00));
+            device.SendCommand(new Commands.Ssd1306Commands.SetChargePump(true));
+            device.SendCommand(
+                new Commands.Ssd1306Commands.SetMemoryAddressingMode(Commands.Ssd1306Commands.SetMemoryAddressingMode.AddressingMode
+                    .Horizontal));
+            device.SendCommand(new Commands.Ssd1306Commands.SetSegmentReMap(true));
+            device.SendCommand(new Commands.Ssd1306Commands.SetComOutputScanDirection(false));
+            device.SendCommand(new Commands.Ssd1306Commands.SetComPinsHardwareConfiguration(false, false));
+            device.SendCommand(new SetContrastControlForBank0(0x8F));
+            device.SendCommand(new Commands.Ssd1306Commands.SetPreChargePeriod(0x01, 0x0F));
+            device.SendCommand(
+                new Commands.Ssd1306Commands.SetVcomhDeselectLevel(Commands.Ssd1306Commands.SetVcomhDeselectLevel.DeselectLevel.Vcc1_00));
+            device.SendCommand(new Commands.Ssd1306Commands.EntireDisplayOn(false));
+            device.SendCommand(new Commands.Ssd1306Commands.SetNormalDisplay());
+            device.SendCommand(new SetDisplayOn());
+            device.SendCommand(new Commands.Ssd1306Commands.SetColumnAddress());
+            device.SendCommand(new Commands.Ssd1306Commands.SetPageAddress(Commands.Ssd1306Commands.PageAddress.Page1,
+                Commands.Ssd1306Commands.PageAddress.Page3));
+        }
+
+        /// <summary>
+        /// Initializes the display for 96x96
+        /// </summary>
+        public static void Initialize(this Ssd1327 device)
+        {
+            device.SendCommand(new Commands.Ssd1327Commands.SetUnlockDriver(true));
+            device.SendCommand(new SetDisplayOff());
+            device.SendCommand(new SetMultiplexRatio(0x5F));
+            device.SendCommand(new Commands.Ssd1327Commands.SetDisplayStartLine());
+            device.SendCommand(new Commands.Ssd1327Commands.SetDisplayOffset(0x5F));
+            device.SendCommand(new Commands.Ssd1327Commands.SetReMap());
+            device.SendCommand(new Commands.Ssd1327Commands.SetInternalVddRegulator(true));
+            device.SendCommand(new SetContrastControlForBank0(0x53));
+            device.SendCommand(new Commands.Ssd1327Commands.SetPhaseLength(0X51));
+            device.SendCommand(new Commands.Ssd1327Commands.SetDisplayClockDivideRatioOscillatorFrequency(0x01, 0x00));
+            device.SendCommand(new Commands.Ssd1327Commands.SelectDefaultLinearGrayScaleTable());
+            device.SendCommand(new Commands.Ssd1327Commands.SetPreChargeVoltage(0x08));
+            device.SendCommand(new Commands.Ssd1327Commands.SetComDeselectVoltageLevel(0X07));
+            device.SendCommand(new Commands.Ssd1327Commands.SetSecondPreChargePeriod(0x01));
+            device.SendCommand(new Commands.Ssd1327Commands.SetSecondPreChargeVsl(true));
+            device.SendCommand(new Commands.Ssd1327Commands.SetNormalDisplay());
+            device.SendCommand(new DeactivateScroll());
+            device.SendCommand(new SetDisplayOn());
+            device.SendCommand(new Commands.Ssd1327Commands.SetRowAddress());
+            device.SendCommand(new Commands.Ssd1327Commands.SetColumnAddress());
+        }
+
+        /// <summary>
+        /// Clears the display
+        /// </summary>
+        public static void ClearScreen(this Ssd1306 device)
+        {
+            device.SendCommand(new Commands.Ssd1306Commands.SetColumnAddress());
+            device.SendCommand(new Commands.Ssd1306Commands.SetPageAddress(Commands.Ssd1306Commands.PageAddress.Page0,
+                Commands.Ssd1306Commands.PageAddress.Page3));
+
+            for (int cnt = 0; cnt < 32; cnt++)
+            {
+                byte[] data = new byte[16];
+                device.SendData(data);
+            }
+        }
+
+        /// <summary>
+        /// Clears the display
+        /// </summary>
+        /// <param name="device">The device</param>
+        public static void ClearScreen(this Ssd1327 device)
+        {
+            device.ClearDisplay();
+        }
+
+        /// <summary>
+        /// Writes text to the display
+        /// </summary>
+        /// <param name="device">The display</param>
+        /// <param name="message">The text</param>
+        public static void SendMessage(this Ssd1306 device, string message)
+        {
+            device.SendCommand(new Commands.Ssd1306Commands.SetColumnAddress());
+            device.SendCommand(new Commands.Ssd1306Commands.SetPageAddress(Commands.Ssd1306Commands.PageAddress.Page0,
+                Commands.Ssd1306Commands.PageAddress.Page3));
+
+            foreach (char character in message)
+            {
+                device.SendData(BasicFont.GetCharacterBytes(character));
+            }
+        }
+
+        /// <summary>
+        /// Writes text to the display
+        /// </summary>
+        /// <param name="device">The display</param>
+        /// <param name="message">The text</param>
+        public static void SendMessageSsd1327(this Ssd1327 device, string message)
+        {
+            device.SetRowAddress(0x00, 0x07);
+
+            foreach (char character in message)
+            {
+                byte[] charBitMap = BasicFont.GetCharacterBytes(character);
+                List<byte> data = new List<byte>();
+                for (var i = 0; i < charBitMap.Length; i = i + 2)
+                {
+                    for (var j = 0; j < 8; j++)
+                    {
+                        byte cdata = 0x00;
+                        int bit1 = (byte)((charBitMap[i] >> j) & 0x01);
+                        cdata |= (bit1 == 1) ? (byte)0xF0 : (byte)0x00;
+                        var secondBitIndex = i + 1;
+                        if (secondBitIndex < charBitMap.Length)
+                        {
+                            int bit2 = (byte)((charBitMap[i + 1] >> j) & 0x01);
+                            cdata |= (bit2 == 1) ? (byte)0x0F : (byte)0x00;
+                        }
+
+                        data.Add(cdata);
+                    }
+                }
+
+                device.SendData(data.ToArray());
+            }
+        }
+
+    }
+}
--- src/devices/Ssd13xx/samples/Program.cs
+++ src/devices/Ssd13xx/samples/Program.cs
@@ -10,6 +10,7 @@ using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Threading;
 using System.IO;
+using Iot.Device.Arduino;
 using SixLabors.Fonts;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
@@ -31,13 +32,13 @@ ClearScreenSsd1327(device);
 SendMessage(device, DisplayIpAddress());
 #else
 using Ssd1306 device = GetSsd1306WithI2c();
-InitializeSsd1306(device);
-ClearScreenSsd1306(device);
+device.Initialize();
+device.ClearScreen();
 // SendMessage(device, "Hello .NET IoT!!!");
 // SendMessage(device, DisplayIpAddress());
 DisplayImages(device);
 DisplayClock(device);
-ClearScreenSsd1306(device);
+device.ClearScreen();
 #endif
 
 I2cDevice GetI2CDevice()
@@ -56,118 +57,6 @@ Ssd1327 GetSsd1327WithI2c()
 Ssd1306 GetSsd1306WithI2c()
 {
     return new Ssd1306(GetI2CDevice());
-}
-
-// Display size 128x32.
-void InitializeSsd1306(Ssd1306 device)
-{
-    device.SendCommand(new SetDisplayOff());
-    device.SendCommand(new Ssd1306Cmnds.SetDisplayClockDivideRatioOscillatorFrequency(0x00, 0x08));
-    device.SendCommand(new SetMultiplexRatio(0x1F));
-    device.SendCommand(new Ssd1306Cmnds.SetDisplayOffset(0x00));
-    device.SendCommand(new Ssd1306Cmnds.SetDisplayStartLine(0x00));
-    device.SendCommand(new Ssd1306Cmnds.SetChargePump(true));
-    device.SendCommand(
-        new Ssd1306Cmnds.SetMemoryAddressingMode(Ssd1306Cmnds.SetMemoryAddressingMode.AddressingMode
-            .Horizontal));
-    device.SendCommand(new Ssd1306Cmnds.SetSegmentReMap(true));
-    device.SendCommand(new Ssd1306Cmnds.SetComOutputScanDirection(false));
-    device.SendCommand(new Ssd1306Cmnds.SetComPinsHardwareConfiguration(false, false));
-    device.SendCommand(new SetContrastControlForBank0(0x8F));
-    device.SendCommand(new Ssd1306Cmnds.SetPreChargePeriod(0x01, 0x0F));
-    device.SendCommand(
-        new Ssd1306Cmnds.SetVcomhDeselectLevel(Ssd1306Cmnds.SetVcomhDeselectLevel.DeselectLevel.Vcc1_00));
-    device.SendCommand(new Ssd1306Cmnds.EntireDisplayOn(false));
-    device.SendCommand(new Ssd1306Cmnds.SetNormalDisplay());
-    device.SendCommand(new SetDisplayOn());
-    device.SendCommand(new Ssd1306Cmnds.SetColumnAddress());
-    device.SendCommand(new Ssd1306Cmnds.SetPageAddress(Ssd1306Cmnds.PageAddress.Page1,
-        Ssd1306Cmnds.PageAddress.Page3));
-}
-
-// Display size 96x96.
-void InitializeSsd1327(Ssd1327 device)
-{
-    device.SendCommand(new Ssd1327Cmnds.SetUnlockDriver(true));
-    device.SendCommand(new SetDisplayOff());
-    device.SendCommand(new SetMultiplexRatio(0x5F));
-    device.SendCommand(new Ssd1327Cmnds.SetDisplayStartLine());
-    device.SendCommand(new Ssd1327Cmnds.SetDisplayOffset(0x5F));
-    device.SendCommand(new Ssd1327Cmnds.SetReMap());
-    device.SendCommand(new Ssd1327Cmnds.SetInternalVddRegulator(true));
-    device.SendCommand(new SetContrastControlForBank0(0x53));
-    device.SendCommand(new Ssd1327Cmnds.SetPhaseLength(0X51));
-    device.SendCommand(new Ssd1327Cmnds.SetDisplayClockDivideRatioOscillatorFrequency(0x01, 0x00));
-    device.SendCommand(new Ssd1327Cmnds.SelectDefaultLinearGrayScaleTable());
-    device.SendCommand(new Ssd1327Cmnds.SetPreChargeVoltage(0x08));
-    device.SendCommand(new Ssd1327Cmnds.SetComDeselectVoltageLevel(0X07));
-    device.SendCommand(new Ssd1327Cmnds.SetSecondPreChargePeriod(0x01));
-    device.SendCommand(new Ssd1327Cmnds.SetSecondPreChargeVsl(true));
-    device.SendCommand(new Ssd1327Cmnds.SetNormalDisplay());
-    device.SendCommand(new DeactivateScroll());
-    device.SendCommand(new SetDisplayOn());
-    device.SendCommand(new Ssd1327Cmnds.SetRowAddress());
-    device.SendCommand(new Ssd1327Cmnds.SetColumnAddress());
-}
-
-void ClearScreenSsd1306(Ssd1306 device)
-{
-    device.SendCommand(new Ssd1306Cmnds.SetColumnAddress());
-    device.SendCommand(new Ssd1306Cmnds.SetPageAddress(Ssd1306Cmnds.PageAddress.Page0,
-        Ssd1306Cmnds.PageAddress.Page3));
-
-    for (int cnt = 0; cnt < 32; cnt++)
-    {
-        byte[] data = new byte[16];
-        device.SendData(data);
-    }
-}
-
-void ClearScreenSsd1327(Ssd1327 device)
-{
-    device.ClearDisplay();
-}
-
-void SendMessageSsd1306(Ssd1306 device, string message)
-{
-    device.SendCommand(new Ssd1306Cmnds.SetColumnAddress());
-    device.SendCommand(new Ssd1306Cmnds.SetPageAddress(Ssd1306Cmnds.PageAddress.Page0,
-        Ssd1306Cmnds.PageAddress.Page3));
-
-    foreach (char character in message)
-    {
-        device.SendData(BasicFont.GetCharacterBytes(character));
-    }
-}
-
-void SendMessageSsd1327(Ssd1327 device, string message)
-{
-    device.SetRowAddress(0x00, 0x07);
-
-    foreach (char character in message)
-    {
-        byte[] charBitMap = BasicFont.GetCharacterBytes(character);
-        List<byte> data = new List<byte>();
-        for (var i = 0; i < charBitMap.Length; i = i + 2)
-        {
-            for (var j = 0; j < 8; j++)
-            {
-                byte cdata = 0x00;
-                int bit1 = (byte)((charBitMap[i] >> j) & 0x01);
-                cdata |= (bit1 == 1) ? (byte)0xF0 : (byte)0x00;
-                var secondBitIndex = i + 1;
-                if (secondBitIndex < charBitMap.Length)
-                {
-                    int bit2 = (byte)((charBitMap[i + 1] >> j) & 0x01);
-                    cdata |= (bit2 == 1) ? (byte)0x0F : (byte)0x00;
-                }
-
-                data.Add(cdata);
-            }
-        }
-
-        device.SendData(data.ToArray());
-    }
 }
 
 string DisplayIpAddress()
@@ -191,7 +80,7 @@ void DisplayImages(Ssd1306 ssd1306)
     {
         using Image<L16> image = Image.Load<L16>(image_name);
         ssd1306.DisplayImage(image);
-        Thread.Sleep(1000);
+        Thread.Sleep(5000);
     }
 }
 
