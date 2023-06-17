ef0bbbf606e76972663da08a2be732ced2a98176
Patrick Grawehr <pgrawehr@hotmail.com>
2023-06-17 21:34:53 +0200

    A raw Maui App
    Very difficult to get this to run, even on Windows

---
+++ samples/ShipSensors/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ShipSensors
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ samples/ShipSensors/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ShipSensors
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
