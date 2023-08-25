f82d6554b3a7a3c2241000974ecfebf1608f59bd
Patrick Grawehr <patrick.grawehr@leica-geosystems.com>
2023-08-25 21:59:59 +0200

    This needs testing

---
+++ samples/DisplayControl/Views/AisTargetViewControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DisplayControl.Views
+{
+    public class SensorValueControl : UserControl
+    {
+        public SensorValueControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
